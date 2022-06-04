20a1d0f4c10e981a59d2312d7be168f0ad961022
Josh Reel <joshreel2@gmail.com>
2022-06-03 22:26:14 -0500

    another one

--- Mapping_GeoJSON_linestrings_2/static/js/logic.js
+++ Mapping_GeoJSON_linestrings_2/static/js/logic.js
@@ -20,7 +20,7 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44.0, -80.0],
   zoom: 2,
   layers: [light]
 })
@@ -28,15 +28,28 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/jmreel/Mapping_Earthquakes/Mapping_GeoJSON_linestrings_2/torontoRoutes.json";
+
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/jmreel/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+L.geoJSON(data, {
+  color: "#ffffa1",
+  weight: 2,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination:"
+    + feature.properties.dst + "</h3>");
+  }
+})
+.addTo(map);
 });
+
 
 
 
