cea636f61d407ef3bdc24d1f4c19b548220835be
Josh Reel <joshreel2@gmail.com>
2022-06-03 22:15:11 -0500

    adding toronto Routes

--- Mapping_GeoJSON_linestrings_2/static/js/logic.js
+++ Mapping_GeoJSON_linestrings_2/static/js/logic.js
@@ -1,5 +1,5 @@
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -14,7 +14,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Street: light,
   Dark: dark
 };
 
@@ -22,7 +22,7 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [30, 30],
   zoom: 2,
-  layers: [streets]
+  layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
